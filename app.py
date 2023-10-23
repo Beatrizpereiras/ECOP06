b599fff20f008980038e6cccc0ec9df942c35135
Beatrizpereiras <146854060+Beatrizpereiras@users.noreply.github.com>
2023-10-23 12:15:41 +0000

    Novo

--- app.py
+++ app.py
@@ -6,6 +6,7 @@ st.set_page_config('Beatriz - ECOP06',
                    'https://unifei.edu.br/wp-content/themes/twentytwelve-child/img/cabecalho/logo-unifei-oficial.png', ) 
 
 st.title('Pagina Demo ECOP06')
-esportes = pd.read_csv('https://github.com/MainakRepositor/Datasets/raw/master/GeneralEsportData.csv')
+esportes = pd.read_csv('https://github.com/MainakRepositor/Datasets/raw/master/GeneralEsportData.csv', 
+                       encoding= 'latin-1')
 
 st.dataframe(esportes)
